cd4e9d9e772ddf9d5322d67b589e7139b920710a
patill <till.paala@gmx.com>
2019-02-15 11:25:13 +0200

    Add ability to show also the converted bar codes

--- js/main.js
+++ js/main.js
@@ -6,11 +6,8 @@ document.getElementById('post-button').addEventListener('click', function () {
 $("#card-stack").empty();//remove old entries and get div empty for new list
 var post = document.createElement('p');
 var postText = document.getElementById('post-text').value;
-var pisteet = /(AU|PA|PS|RA|PL|KÄ|PN|RE|PK)/g;
-var nega = /609[AUPSLKR]/g
-var onKunta = postText;
-//post.textContent = postText;
-
+var pisteet = /(AU|PA|PS|RA|PL|KÄ|PN|RE|PK|HU)/g;
+var nega = /609[AUPSLKHR]/g
 
 var postArray = postText.split('\n');
 var i = 0;
@@ -19,12 +16,11 @@ var filtered = []; //empty array
 for (i = 0; i < l; i++) {
   if (!nega.test(postArray[i])) { //takes only those not having 609+letter
     filtered[filtered.length] = postArray[i].replace(pisteet, '609$&');//adding to the arry
-  //  filtered = filtered.push(postArray[i]);
   }
   else {
     filtered[filtered.length] = postArray[i]; //adds the "wrong" ones anyway
   }
-}
+};
 
 post.textContent = filtered.toString();
 post.innerHTML = post.innerHTML.replace(/,/g, '<br>\n').replace(/^ /g, "");  // <-- THIS FIXES THE LINE BREAKS
@@ -33,5 +29,19 @@ card.appendChild(post);
 var cardStack = document.getElementById('card-stack');
 cardStack.insertBefore(card, cardStack.firstChild);
 $("#card-stack").show();
+
+for (i = 0; i < l; i++) {
+  var output = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  output.className.baseVal = "barcode";
+  output.setAttribute('jsbarcode-value',  filtered[i].trim());
+  output.setAttribute('jsbarcode-format', 'code39');
+
+  var div_element = document.getElementById("wrapper");
+  div_element.appendChild(output);
+  var newline = document.createElement("br");
+  div_element.appendChild(newline);
+  JsBarcode(".barcode").init();
+};
+
 });
 });
